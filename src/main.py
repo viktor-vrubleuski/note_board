16d7103a8f083f46433c2cf92abddc0e9e589309
Viktar Vrubleuski <viktorvrubleuski@gmail.com>
2023-08-03 13:25:56 +0300

    add logging & test

--- src/main.py
+++ src/main.py
@@ -1,11 +1,38 @@
+import logging.config
+import random
+import string
+import time
+
 import sqlalchemy
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from src.app.api import board, note
 
 from .app.db import DeclarativeBase, database, dialect
 
+logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
+
+logger = logging.getLogger(__name__)
+
 app = FastAPI()
+
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    idem = "".join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    logger.info(f"rid={idem} start request path={request.url.path}")
+    start_time = time.time()
+
+    response = await call_next(request)
+
+    process_time = (time.time() - start_time) * 1000
+    formatted_process_time = "{:.2f}".format(process_time)
+    logger.info(
+        f"rid={idem} completed_in={formatted_process_time}ms "
+        f"status_code={response.status_code}"
+    )
+
+    return response
 
 
 @app.on_event("startup")
